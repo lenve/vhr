20d8a45f17e9c8757a93008c1547817d0359a3f7
zhangsean <zxf2342@qq.com>
2018-10-06 02:50:24 +0800

    浏览器打开项目时引导到登录页

--- hrserver/src/main/java/org/sang/controller/RegLoginController.java
+++ hrserver/src/main/java/org/sang/controller/RegLoginController.java
@@ -3,6 +3,7 @@ package org.sang.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,13 +14,16 @@ import java.io.PrintWriter;
 @RestController
 public class RegLoginController {
     @RequestMapping("/login_p")
-    public void login(HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        resp.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.write("{\"status\":\"error\",\"msg\":\"尚未登录，请登录!\"}");
-        out.flush();
-        out.close();
-        //        return new RespBean("error", "尚未登录，请登录!");
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getHeader("Accept").contains("text/html")) {
+            resp.sendRedirect("index.html");
+        } else {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.setContentType("application/json;charset=UTF-8");
+            PrintWriter out = resp.getWriter();
+            out.write("{\"status\":\"error\",\"msg\":\"尚未登录，请登录!\"}");
+            out.flush();
+            out.close();
+        }
     }
 }
