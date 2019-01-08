e248f3e33b0ad990fe00d3b03b1afe2b5a397b17
江南一点雨 <wangsong0210@gmail.com>
2019-01-08 15:21:02 +0800

    更新过时API

--- hrserver/src/main/java/org/sang/config/WebSocketConfig.java
+++ hrserver/src/main/java/org/sang/config/WebSocketConfig.java
@@ -5,13 +5,14 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
  * Created by sang on 2018/1/27.
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         stompEndpointRegistry.addEndpoint("/ws/endpointChat").withSockJS();
