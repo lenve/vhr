7f27d3b87e9848aeb24d8284e284fe4365467d69
江南一点雨 <wangsong0210@gmail.com>
2020-02-28 23:30:31 +0800

    添加登录验证码

--- vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/controller/LoginController.java
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/controller/LoginController.java
@@ -1,9 +1,14 @@
 package org.javaboy.vhr.controller;
 
+import org.javaboy.vhr.config.VerificationCode;
 import org.javaboy.vhr.model.RespBean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * @作者 江南一点雨
@@ -19,5 +24,14 @@ public class LoginController {
     @GetMapping("/login")
     public RespBean login() {
         return RespBean.error("尚未登录，请登录!");
+    }
+
+    @GetMapping("/verifyCode")
+    public void verifyCode(HttpSession session, HttpServletResponse resp) throws IOException {
+        VerificationCode code = new VerificationCode();
+        BufferedImage image = code.getImage();
+        String text = code.getText();
+        session.setAttribute("verify_code", text);
+        VerificationCode.output(image,resp.getOutputStream());
     }
 }
